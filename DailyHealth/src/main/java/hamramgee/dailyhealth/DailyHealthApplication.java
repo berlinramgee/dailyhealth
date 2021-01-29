f90f128f4d66b3043276f406b40f30f8a5ff5c25
berlinramgee <berlin.ramgee@gmail.com>
2021-01-29 18:38:42 +0800

    First commit

---
+++ DailyHealth/src/main/java/hamramgee/dailyhealth/DailyHealthApplication.java
@@ -0,0 +1,13 @@
+package hamramgee.dailyhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyHealthApplication.class, args);
+	}
+
+}
---
+++ DailyHealth/src/test/java/hamramgee/dailyhealth/DailyHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package hamramgee.dailyhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
